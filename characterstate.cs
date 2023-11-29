b1a772745d8503c4c801c3ffaef29f09512d7b70
leafduck <141562827+leafduck@users.noreply.github.com>
2023-11-29 14:00:21 +0900

    Update characterstate.cs

--- characterstate.cs
+++ characterstate.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "characterstate", menuName = "test/characterlist", order = 0)]
 public class characterstate : ScriptableObject {
+    [field: SerializeField]
     public const int name
     {
         private set;
     }
+    [field: SerializeField]
     public const int age;
     {
         private set;
