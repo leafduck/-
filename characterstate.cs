d6b50bf1ebbdb0ebd685b02ec700bf8eb110489f
leafduck <141562827+leafduck@users.noreply.github.com>
2023-11-29 14:01:49 +0900

    Update characterstate.cs

--- characterstate.cs
+++ characterstate.cs
@@ -3,26 +3,30 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "characterstate", menuName = "test/characterlist", order = 0)]
 public class characterstate : ScriptableObject {
     [field: SerializeField]
-    public const int name
+    public int name//이름
     {
         private set;
     }
     [field: SerializeField]
-    public const int age;
+    public int age;//나이
     {
         private set;
     }
-    public enum Gender{
+    public enum Gender{//성별
         male,
         female
     }
-    public const Gender gender;
-    public enum Personailty{
+    public Gender gender;
+    public enum Personailty{//성격
         stupid,
         suspicious,
         clever
 
     }
-    public const Personailty personailty;
+    public Personailty personailty;
+    public enum RT{//추리성향
+        //....
+    }
+    public RT rt;
 
 }
