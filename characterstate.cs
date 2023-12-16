3122276484e57c1301679fea72b1f7adf2e5bbab
leafduck <141562827+leafduck@users.noreply.github.com>
2023-12-17 00:30:55 +0900

    Update characterstate.cs

--- characterstate.cs
+++ characterstate.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "characterstate", menuName = "test/characterlist", order = 0)]
 public class characterstate : ScriptableObject {
-    [field: SerializeField]
+    [field: SerializeField]   // 자동 구현 프로퍼티에 사용하는 serializefield 어트리뷰트이다.
     public int name//이름
     {
         private set;
     }
-    [field: SerializeField]
     public int age;//나이
-    {
-        private set;
-    }
     public enum Gender{//성별
         male,
         female
@@ -21,13 +17,12 @@ public class characterstate : ScriptableObject {
         stupid,
         suspicious,
         clever
-
     }
     public Personailty personailty;
-    public enum RT{//추리성향
+    public enum Tendency{//추리성향
         //....
     }
-    public RT rt;
+    public Tendency tendency;
     //npc 상태
     public enum situation{//상태
         //....
