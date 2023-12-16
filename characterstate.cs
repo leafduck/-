5389859d02b431bab751598cc46af4e27aaa0fd2
leafduck <141562827+leafduck@users.noreply.github.com>
2023-12-17 01:31:36 +0900

    Update characterstate.cs

--- characterstate.cs
+++ characterstate.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "characterstate", menuName = "test/characterlist", order = 0)]
 public class characterstate : ScriptableObject {
+    [field: SerializeField]
+    public int reliability;//신뢰도
+    {
+        get;
+    }
+    {
+        set;
+    }
     [field: SerializeField]   // 자동 구현 프로퍼티에 사용하는 serializefield 어트리뷰트이다.
     public int name//이름
     {
@@ -9,17 +17,17 @@ public class characterstate : ScriptableObject {
     }
     public int age;//나이
     public enum Gender{//성별
-        male,
-        female
+        Male,
+        Female
     }
     public Gender gender;
     public enum Personailty{//성격(정보수용태도에 영향을 준다.) 개임 내에서 조회가 불가능하다.
-        cautious, //조심스러운
+        Cautious, //조심스러운
         Suspicious, //의심많은
         Clever, //영리한
         Impatient, // 성급한
-        cunning, //교활한
-        reticent //과묵한
+        Cunning, //교활한
+        Reticent //과묵한
         
     }
     public Personailty personailty;
@@ -28,7 +36,7 @@ public class characterstate : ScriptableObject {
         Strange, //괴짜
         Cooperately, //협동적
         Newtral, //중립
-        lead //리더
+        Lead //리더
         
     }
     public Tendency tendency;
@@ -36,13 +44,14 @@ public class characterstate : ScriptableObject {
     public enum Situation{//상태
         Doubtful, //의심중
         Uneasy, //불안한
-        panic,// 공황
-        confidentable,  //자신감있는
+        Panic,// 공황
+        Confidentable,  //자신감있는
         Comportable, //편안한(기본상태)
-        thinking, //생각 중
-        composure, //침착함
-        excitement, //흥분한
-        glad, //기쁜
+        Thinking, //생각 중
+        Composure, //침착함
+        Excitement, //흥분한
+        Glad, //기쁜
+        Sad, //슬픈
         Zonked, //기운빠진
         Sympathy //동정하는
 
