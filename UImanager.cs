79bc12ef6842e0af1db187b3c1f41137352d1622
leafduck <141562827+leafduck@users.noreply.github.com>
2023-12-19 23:53:06 +0900

    Update and rename anyscript.cs to UImanager.cs

---
+++ UImanager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    void start{
+    
+    }
+    void Update{
+    
+    }
+
+
+
+
+
+
+
+}
