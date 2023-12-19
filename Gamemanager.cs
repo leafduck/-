82bc484bf1ecadbe670316486eafc7c0319009df
leafduck <141562827+leafduck@users.noreply.github.com>
2023-12-20 00:17:05 +0900

    Update Gamemanager.cs

--- Gamemanager.cs
+++ Gamemanager.cs
@@ -12,27 +12,57 @@ public class Gamemanager : MonoBehaviour
     전반적으로 게임 시작과 종료를 구현할거다. uimanager에서 정의한 메서드들을 사용할 예정이다.
     씬로드는 따로 구현할 예정이다. 씬로드를 호출한다.
     */ 
-    public static Gamemanager instance;
-    public int killcount;//킬 카운트
-    public int nigritude;//악명(최대 100까지 있다.)
-
-
-    public bool isGameEnding=false;//죽거나 특정 조건을 충족하면 엔딩
+    public 
+    private static Gamemanager _instance;
     public bool isGameOver=false;//죽으면 게임오버
-    
-    private void Awake() {
-        if(instance==null){
-            instance=this;
+    private int count;
+    {
+        set
+        {
+            if(count==)
         }
-        else{Destroy(gameObject);}
+    
+    }
+    public static Gamemanager Instance//외부에서 호출할 때 쓰는 프로퍼티
+    {
+        get
+        {
+            // 인스턴스가 없으면 새로 생성
+            if (_instance == null)
+            {
+                // 씬에서 싱글톤 오브젝트를 찾아봅니다.
+                _instance = FindObjectOfType<MySingleton>();
+
+                // 씬에 없으면 새로 생성
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject("Gamemanager");
+                    _instance = singletonObject.AddComponent<MySingleton>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+    private void Awake() {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            // 처음 생성된 인스턴스를 저장
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
     
     private void Start() {
             
     }
     private void Update() {
-        if(nigritude>100||nigritude<0){renewalnoto();}//명성범위제한 함수 실행
+         count++;
         
     }
-    void renewalnoto()=>nigritude=Mathf.Clamp(nigritude,0,100);//명성범위제한 함수 구현
+    
 }
