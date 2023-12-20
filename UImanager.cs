1c8c92eaf40857d0f4a6b5c57760de773474b2c8
leafduck <141562827+leafduck@users.noreply.github.com>
2023-12-20 14:36:51 +0900

    Update UImanager.cs

--- UImanager.cs
+++ UImanager.cs
@@ -41,7 +41,9 @@ public class UImanager : MonoBehaviour// 여기에서는 함수구현만 하고,
             DontDestroyOnLoad(this.gameObject);
         }    
     }
-    void WriteNotification(string notice){
+    IEnumerater WriteNotification(string notice){
         NotificationText=notice;
+        yield return new waitforsecond(2f);
+        NotificationText="";
     }
 }
