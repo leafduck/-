be6ecec2e1162941de73bfc8827ca6ce9dd0eec4
leafduck <141562827+leafduck@users.noreply.github.com>
2023-12-21 13:40:39 +0900

    Update playermove.cs

--- playermove.cs
+++ playermove.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
- 
-public class playermove : MonoBehaviour {
- 
-    float speed = 10f;
-    Rigidbody rigidbody;
-    Vector2 movement;
-    float horizontal, vertical;
 
-    void Start()=>rigidbody = GetComponent<Rigidbody>();
-    
+public class playermove : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 이동 속도 조절 변수
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-    }
-    void FixedUpdate()
-    {
-        movement.Set(horizontal, vertical, 0f);
-        movement = movement.normalized * speed * Time.deltaTime;
-        rigidbody.MovePosition(transform.position + movement);
+        // 수평 및 수직 입력을 받아 이동 방향을 설정
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 moveDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+        // 이동 방향에 따라 실제 이동 처리
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
